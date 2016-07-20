e0c0ec1d8093068c35fc6335943d03b5afe33670
Francisco Rafael Leon <frleon@ucdavis.edu>
2016-07-20 15:16:52 -0400

    Socket.io Broadcast of Vuex Mutations

--- server.js
+++ server.js
@@ -34,36 +34,39 @@ app.use(convert(serve('node_modules/vue/dist')));
 app.use(convert(serve('node_modules/vuex/dist')));
 app.use(convert(serve('www'+env)));
 
+// Listeners for socket.io events
 io.on('connection',co.wrap(function *(ctx) {
   console.log("New socket connection with id: "+ctx.socket.id);
   var db = yield comongo.connect('mongodb://localhost/sessions');
   ctx.socket.itemscoll = yield db.collection('items');
 }));
 
-io.on('mutation',co.wrap(function *(ctx,mutation) {
+io.on('ADDITEM',co.wrap(function *(ctx,payload) {
   var itemscoll = ctx.socket.socket.itemscoll;
-  switch(mutation.type){
-    case 'ADDITEM':
-      yield itemscoll.update({desc:mutation.payload},
-                             {$inc:{num:1}},
-                             {upsert:true});
-      break;
-    case 'REMITEM':
-      yield itemscoll.update({desc:mutation.payload},
-                             {$inc:{num:-1}});
-      yield itemscoll.remove({desc:mutation.payload,num:0});
-      break;
-    case 'REMALL':
-      yield itemscoll.remove({});
-      break;
-    default:
-      console.log('Invalid mutation.');
-  }}));
+  yield itemscoll.update({desc:payload},
+                         {$inc:{num:1}},
+                         {upsert:true});
+  ctx.socket.broadcast('$ADDITEM',payload);
+}));
+
+io.on('REMITEM',co.wrap(function *(ctx,payload) {
+  var itemscoll = ctx.socket.socket.itemscoll;
+  yield itemscoll.update({desc:payload},
+                         {$inc:{num:-1}});
+  yield itemscoll.remove({desc:payload,num:0});
+  ctx.socket.broadcast('$REMITEM',payload);
+}));
+
+io.on('REMALL',co.wrap(function *(ctx) {
+  var itemscoll = ctx.socket.socket.itemscoll;
+  yield itemscoll.remove({});
+  ctx.socket.broadcast('$REMALL');
+}));
 
 io.on('refresh', co.wrap(function* (ctx) {
   var itemscoll = ctx.socket.socket.itemscoll;
   var allitems = yield itemscoll.find({},{_id:0}).toArray();
-  ctx.socket.emit('setstate',allitems);
+  ctx.socket.emit('$SETSTATE',allitems);
 }));
 
 var port = 3000;
--- wwwdev/js/store.js
+++ wwwdev/js/store.js
@@ -2,46 +2,65 @@
 const state = {
   items: []
 }
+ 
+// All mutations starting with '$' are for remote use.
+function $SETSTATE (state,items) {
+  state.items=items;
+}
 
-const mutations = { 
-  SETSTATE (state,items) {
-    state.items=items;
-  },
-  ADDITEM (state,item) {
-    var thisitem = state.items.find(i => { return (i.desc == item);});
-    if(thisitem) {thisitem.num++}
-    else {
-      state.items.push({desc: item, num:1});
-    }
-  },
-  REMITEM (state,item) {
-    var itemindex=state.items.findIndex(i => { return (i.desc == item);});
-    if(itemindex!=-1){
-      var thisitem = state.items[itemindex];
-      if(thisitem.num>1) { thisitem.num--;}
-      else {
-        state.items.splice(itemindex,1);
-      }
-    }},
-  REMALL (state) {
-    state.items=[];
+function ADDITEM (state,item) {
+  var thisitem = state.items.find(i => { return (i.desc == item);});
+  if(thisitem) {thisitem.num++}
+  else {
+    state.items.push({desc: item, num:1});
   }
 }
 
+var $ADDITEM = ADDITEM;
+
+function REMITEM (state,item) {
+  var itemindex=state.items.findIndex(i => { return (i.desc == item);});
+  if(itemindex!=-1){
+    var thisitem = state.items[itemindex];
+    if(thisitem.num>1) { thisitem.num--;}
+    else {
+      state.items.splice(itemindex,1);
+    }}}
+
+var $REMITEM = REMITEM;
+
+function REMALL (state) {
+  state.items=[];
+}
+
+var $REMALL = REMALL;
+
+const mutations = {$SETSTATE,
+                   ADDITEM,
+                   $ADDITEM,
+                   REMITEM,
+                   $REMITEM,
+                   REMALL,
+                   $REMALL}
+
 var socket = io();
 const sioplugin = store => {
-  store.subscribe((mutation, state) => {
-    if(mutation.type != 'SETSTATE'){
-      socket.emit('mutation',mutation);
-    }
-  })
-  socket.on('setstate',items => {
-    store.commit('SETSTATE',items);
-  });
+  store.subscribe((mutation) => {
+    if(mutation.type[0] != '$'){
+      socket.emit(mutation.type,mutation.payload);
+    }});
+  socket.on('$SETSTATE',items => {
+    store.commit('$SETSTATE',items);});
+  socket.on('$ADDITEM',item => {
+    store.commit('$ADDITEM',item);});
+  socket.on('$REMITEM',item => {
+    store.commit('$REMITEM',item);});
+  socket.on('$REMALL', () => {
+    store.commit('$REMALL');});
 }
 
 var refreshstate = function (){
-  socket.emit('refresh',{});
+  socket.emit('refresh');
 }
 
 var store = new Vuex.Store({
